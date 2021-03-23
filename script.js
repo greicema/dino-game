3466c1aadb3a60537dfffdb0ee9aef9dc066fa24
Greice Mara <sgreicemaraj@gmail.com>
2021-03-23 15:53:27 -0300

    movimentando cactos e adicionando tela de game over

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const dino = document.querySelector('.dino');
 const background = document.querySelector('.background');
+
 let isJumping = false;
+let position = 0;
 
 function handleKeyUp(event) {
     if (event.keyCode === 32 || event.keyCode === 38) {
         if (!isJumping) {
             jump();
+            console.log('pulou');
         }
     }
 }
 
 function jump() {
-    let position = 0;
     isJumping = true;
     let upInterval = setInterval(() => {
         if (position >= 150) {
@@ -30,7 +32,7 @@ function jump() {
         } else {
             // Subindo
             position += 20;
-            dino.style.bottom = position + 'px';
+            dino.style.bottom = position + 'px'
         }
     }, 20);
 }
@@ -38,10 +40,30 @@ function jump() {
 function createCactus() {
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000;
+
+    // if (isGameOver) return;
 
     cactus.classList.add('cactus');
-    cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
+    cactus.style.left = 1000 + 'px';
+
+    let leftInterval = setInterval(() => {
+            if (cactusPosition < -60) {
+                clearInterval(leftInterval);
+                background.removeChild(cactus);
+            } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+                //game over
+                clearInterval(leftInterval);
+                document.body.innerHTML = '<h1 class = "game-over" > Fim de jogo </h1>';
+            } else {
+                cactusPosition -= 10;
+                cactus.style.left = cactusPosition + 'px';
+            }
+        },
+        20);
+
+    setTimeout(createCactus, randomTime);
 }
 
 createCactus();
